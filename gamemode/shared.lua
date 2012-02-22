d19e724170878985c1f5e24ecc4e1713ae7a4c49
FrozenGMod <FrozenGMod@gmail.com>
2012-02-22 23:01:56 +0000

    Added base structure

---
+++ gamemode/shared.lua
@@ -0,0 +1,12 @@
+
+GM.Name 	= "SimpleRP"
+GM.Author 	= "Marmotte Unijambiste, Frozen"
+GM.Email 	= "N/A"
+GM.Website 	= "N/A"
+
+function GM:Initialize()
+
+	self.BaseClass.Initialize( self )
+	
+end
+
