6791bbcdcb78205decafeafd82969495018f4f30
FrozenGMod@gmail.com <FrozenGMod@gmail.com@877d49f1-03b8-69c1-2f19-9e6c0e3fe0b8>
2012-02-23 00:09:44 +0000

    Start working on scoreboard.lua

--- trunk/gamemode/init.lua
+++ trunk/gamemode/init.lua
@@ -1,6 +1,15 @@
 
 
+AddCSLuaFile("core/client/scoreboard.lua")
+
 
 function GM:Initialize()
 	print(GM.Name.." enabled")
+end
+
+function GM:HUDShouldDraw(name)
+	if name == "CHudGMod" then
+		return false
+	end
+	return true
 end
