307fe9cc230501e851d8585bdc5c95a790212c8a
FrozenGMod <FrozenGMod@gmail.com>
2012-02-23 03:03:51 +0000

    Removed fucked scoreboard

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -6,6 +6,17 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("core/client/scoreboard.lua")
 AddCSLuaFile("core/client/HUD.lua")
 
+function AddCSLuaDir(fol)
+	Files = file.FindInLua(fol)
+	for k,v in pairs(Files) do
+		if(file.IsDir(v)) then
+				AddCSLuaDir(v)
+		else
+			
+		end
+	end
+end
+
 function GM:Initialize()
 	print("SimpleRP enabled")
 end
