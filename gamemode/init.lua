bcbdd75f02297a6f6e244b54518ed7793bed371c
FrozenGMod <FrozenGMod@gmail.com>
2012-02-23 00:11:56 +0000

    Removed test function

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -6,10 +6,3 @@ AddCSLuaFile("core/client/scoreboard.lua")
 function GM:Initialize()
 	print(GM.Name.." enabled")
 end
-
-function GM:HUDShouldDraw(name)
-	if name == "CHudGMod" then
-		return false
-	end
-	return true
-end
